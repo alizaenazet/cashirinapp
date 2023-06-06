ff9a4e72c1cd91560f3d44fbfe8a50935b6667e6
Azet <alizaenazet@icloud.com>
2023-06-06 17:20:15 +0700

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.alpcashierin.cashierin {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.alpcashierin.cashierin to javafx.fxml;
+    exports com.alpcashierin.cashierin.controller;
+    exports com.alpcashierin.cashierin;
+
+}
